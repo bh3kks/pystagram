bea700aeda8ba81d93a350c557a30cfe36640ce0
KwonKyungsoon <bh3kks@naver.com>
2016-01-24 16:15:30 +0900

    url 추가 및 변경

--- pystagram/urls.py
+++ pystagram/urls.py
@@ -8,23 +8,28 @@ from profiles.urls import urlpatterns as profile_urls
 
 urlpatterns = [
 
+	url(r'^main/$', 'profiles.views.main_view', name='main_view'),
+	# 메인 화면 url
+
+	url(r'^signin/$', 'profiles.views.signin', name='singin'),
+	# 회원가입 화면 rul
+
+	url(r'^login/$', 'profiles.views.login_view', name='login'),
+	# login 화면 url 
+
+	url(r'^login/check/$', 'profiles.views.login_check', name='login_check'),
+	# login 체크 화면 url
+
+    url(r'^accounts/logout/', 'django.contrib.auth.views.logout', name='logout'),
+    # logout 화면 url - 장고에 내장된 logout 함수 사용
+
+
 	url(r'^photo/(?P<photo_id>\d+)/$', 'photo.views.single_photo', name='view_single_photo'),
 	# 하나의 사진 출력 url
 	# url(regex(정규표현식) , view(화면 표시할 함수), name)
 
 	url(r'^photo/upload/$', 'photo.views.new_photo', name='new_photo'),
 	# 사진 업로드 url
-
-	url(r'^accounts/login/', 'django.contrib.auth.views.login', name='login',
-		kwargs={'template_name': 'login.html'}),
-	# login 화면 url - 장고에 내장된 login 함수 사용
-	# kwargs는 해당 login함수에 추가로 인자 전달 : key가 'template_name'이고 값이 'login.html'
-	# login함수에 template_name이 전달되면 login.html을 로그인 화면 출력으로 사용
-
-    url(r'^accounts/logout/', 'django.contrib.auth.views.logout', name='logout'),
-    # logout 화면 url - 장고에 내장된 logout 함수 사용
-
-    url(r'^signin/$', 'photo.views.signin', name='singin'),
 
     url(r'^admin/', include(admin.site.urls)),
     # admin url
