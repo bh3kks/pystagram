672f0f7147f2bac17e6657b59ad5061465f7dfdf
KwonKyungsoon <bh3kks@naver.com>
2016-01-24 16:14:10 +0900

    회원가입 view : profiles 앱으로 이동 로그인 뷰와 아이디,비밀번호를 확인해서 프로필 화면을 띄우는 뷰 추가

--- profiles/views.py
+++ profiles/views.py
@@ -4,6 +4,62 @@ from __future__ import unicode_literals
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth import get_user_model
 
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate
+from django.http import HttpResponseRedirect, HttpResponse
+
+def main_view(request):
+	# 메인 화면 출력
+	return render(request, 'main_view.html')
+
+
+def signin(request):
+
+	if request.method == "GET":
+		userform = UserCreationForm()
+
+	elif request.method == "POST":
+		userform = UserCreationForm(request.POST)
+
+		if userform.is_valid():
+			userform.save()
+
+			return HttpResponse('회원 가입 완료')
+
+	return render(request, "signin.html", {"userform": userform,})
+
+
+def login_view(request):
+	# 로그인 화면 출력
+	return render(request, 'login.html')
+
+def login_check(request):
+	# 아이디, 비밀번호 체크 뷰
+	if 'email' in request.POST:
+		if len(request.POST['email']) == 0:
+			return HttpResponse('글자를 입력하세요.')
+		else:
+			cur_email = request.POST['email']
+	else: 
+		return HttpResponse('False in ID')
+
+	if 'password' in request.POST:
+		if len(request.POST['password']) == 0:
+			return HttpResponse('글자를 입력하세요.')
+		else:
+			cur_password = request.POST['password']
+	else: 
+		return HttpResponse('False in password')
+
+	user = authenticate(username=cur_email, password=cur_password)
+	if user is not None:
+		if user.is_active:
+			return HttpResponseRedirect('/user/%s/' %cur_email)
+		else:
+			return HttpResponse('ACCOUNT DISABLED')
+	else:
+		return HttpResponse('INCORRECT PASSWORD.')
+
 
 def profile(request, username):
 
